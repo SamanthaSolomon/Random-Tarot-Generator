f8c170fe743deca7b6e80714e48e2d9672d315b1
Samantha Solomon <sleahsolomon@gmail.com>
2020-12-17 20:46:01 -0500

    watched hw review

--- random_imager/app.js
+++ random_imager/app.js
@@ -1,49 +1,62 @@
 ////////////// STEP 1 - make div w/ text  ///////////////
 
-const $main = $("main")
-const $div = $("<div>")
-$main.prepend($div)
-$div.text("Add random image")
-$div.addClass("emptyCard")
-
-console.log($div)
-
 const cards = [
-    'https://www.trustedtarot.com/img/cards/the-fool.png',
-    'https://www.trustedtarot.com/img/cards/the-magician.png',
-    'https://www.trustedtarot.com/img/cards/the-high-priestess.png',
-    'https://www.trustedtarot.com/img/cards/the-empress.png',
-    'https://www.trustedtarot.com/img/cards/the-emperor.png',
-    'https://www.trustedtarot.com/img/cards/the-heirophant.png',
-    'https://www.trustedtarot.com/img/cards/the-chariot.png',
-    'https://www.trustedtarot.com/img/cards/strength.png',
-    'https://www.trustedtarot.com/img/cards/the-hermit.png',
-    'https://www.trustedtarot.com/img/cards/wheel-of-fortune.png',
-    'https://www.trustedtarot.com/img/cards/justice.png',
-    'https://www.trustedtarot.com/img/cards/the-hanged-man.png',
-    'https://www.trustedtarot.com/img/cards/death.png',
-    'https://www.trustedtarot.com/img/cards/temperance.png',
-    'https://www.trustedtarot.com/img/cards/the-tower.png',
-    'https://www.trustedtarot.com/img/cards/the-moon.png'
-  ]
+  'https://www.trustedtarot.com/img/cards/the-fool.png',
+  'https://www.trustedtarot.com/img/cards/the-magician.png',
+  'https://www.trustedtarot.com/img/cards/the-high-priestess.png',
+  'https://www.trustedtarot.com/img/cards/the-empress.png',
+  'https://www.trustedtarot.com/img/cards/the-emperor.png',
+  'https://www.trustedtarot.com/img/cards/the-heirophant.png',
+  'https://www.trustedtarot.com/img/cards/the-chariot.png',
+  'https://www.trustedtarot.com/img/cards/strength.png',
+  'https://www.trustedtarot.com/img/cards/the-hermit.png',
+  'https://www.trustedtarot.com/img/cards/wheel-of-fortune.png',
+  'https://www.trustedtarot.com/img/cards/justice.png',
+  'https://www.trustedtarot.com/img/cards/the-hanged-man.png',
+  'https://www.trustedtarot.com/img/cards/death.png',
+  'https://www.trustedtarot.com/img/cards/temperance.png',
+  'https://www.trustedtarot.com/img/cards/the-tower.png',
+  'https://www.trustedtarot.com/img/cards/the-moon.png'
+]
+const randomCard = () => {
+  const index = Math.floor(Math.random() * cards.length)
 
-  // select button
+  return cards[index]
+}
 
-  const $button = $("div")
+const $div = $("<div class= 'emptyCard'> Add Random Image </div>")
 
-  // create click handler function
+const $body = $('body')
 
-  const clickHandler = (event) => {
-// create random generator function
-    const randomCard = ([cards]) => {
-        return Math.floor(Math.random() * cards.length)
-    }
-// use append function to add to page
-    $div.prepend(randomCard)
-  }
+$body.append($div)
+
+$div.on("click", (event) => {
+
+  const $img = $("<img class = 'card'>")
+
+  $img.attr("src", randomCard())
+
+  $body.append($img)
+
+})
 
 
-//TRIED MY BEST BY BREAKING DOWN THIS PROCESS. I NEVER GOT THE DIV TO SHOW UP ON LIVE SERVER, WHICH MADE THIS REALLY HARD. THIS MORNING I THINK MAYBE MAKING ALL THE CARDS LIS WOULD HAVE BEEN A BETTER APPROACH.
+
+//   // select button
+
+//   const $button = $("div")
+
+//   // create click handler function
+
+//   const clickHandler = (event) => {
+// // create random generator function
+    
+// // use append function to add to page
+//     $div.prepend(randomCard)
+//   }
+
+
+// //TRIED MY BEST BY BREAKING DOWN THIS PROCESS. I NEVER GOT THE DIV TO SHOW UP ON LIVE SERVER, WHICH MADE THIS REALLY HARD. THIS MORNING I THINK MAYBE MAKING ALL THE CARDS LIS WOULD HAVE BEEN A BETTER APPROACH.
 
 
 
